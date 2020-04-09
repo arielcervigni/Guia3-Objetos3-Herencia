78abead0caf4142b9522f079b9b56993847a3ea1
Ariel Cervigni <arielcervigni@gmail.com>
2020-04-08 23:03:36 -0300

    Hola Mundo 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hola Mundo2");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola de nuevo 2");
 
     }
 }
